bb0359639c25b2adcda3e16fd8e88b04721d2c7c
raj <rraju12@gmail.com>
2018-08-17 00:41:32 +0100

    Add solutions to the second part of the homework exercises

--- api/class2.js
+++ api/class2.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
-const filename = 'database/database.sqlite';
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database(filename);
+// const customers = require("./customers");
+// const reservations = require("./reservations");
 // db.run("PRAGMA foreign_keys = ON");
-
-router.get('/customers', function(req, res) {
+const filename = "database/database.sqlite";
+const sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database(filename);
+// router.use("/customers", customers);
+// router.use("/reservations", reservations);
+router.get('/customers', function (req, res) {
   var sql = 'select * from customers';
   // var sql = `select * from customers where surname in ("O'Connor", 'Trump')`;
   db.all(sql, [], (err, rows) => {
     if (err) {
-        console.log('ERROR fetching from the database:', err);
-        return;
+      console.log('ERROR fetching from the database:', err);
+      return;
     }
     console.log('Request succeeded, new data fetched', rows);
     res.status(200).json({
@@ -22,7 +24,7 @@ router.get('/customers', function(req, res) {
 });
 
 
-router.get('/customers/:id', function(req, res) {
+router.get('/customers/:id', function (req, res) {
   // TODO: add code here
   const id = req.params.id
   var regx = new RegExp('^[0-9]$');
@@ -45,18 +47,12 @@ router.get('/customers/:id', function(req, res) {
   });
 });
 
-
-// router.get('/customers/:id', function(req, res) {
-//   // TODO: add code here
-// });
-
-
-router.get("/customers/surname/:surname", function(req, res) {
+router.get("/customers/surname/:surname", function (req, res) {
   // TODO: add code here
   console.log("req.params.surname", req.params.surname);
   var sql = `select * from customers where surname like '${
     req.params.surname
-  }'`;
+    }'`;
   console.log("sql", sql);
   db.all(sql, [], (err, rows) => {
     if (err) {
@@ -70,8 +66,8 @@ router.get("/customers/surname/:surname", function(req, res) {
 });
 
 
-router.post('/customers/', function(req, res) {
-  const { title, first_name, surname, email} = req.body
+router.post('/customers/', function (req, res) {
+  const { title, first_name, surname, email } = req.body
   console.log(req.body);
   var sql = `INSERT INTO customers(title, first_name, surname, email) VALUES 
   ('${title}', '${first_name}', '${surname}', '${email}')`;
@@ -81,7 +77,7 @@ router.post('/customers/', function(req, res) {
 });
 
 
-router.put('/customers/:id', function(req, res) {
+router.put('/customers/:id', function (req, res) {
   const { title, first_name, surname, email } = req.body
   var sql = `UPDATE customers 
              SET 
@@ -95,7 +91,7 @@ router.put('/customers/:id', function(req, res) {
 });
 //Exercise 1: updating some fields of a customer
 //I added a new endpoint as I want have it separately
-router.put("/customers/customer/:id", function(req, res) {
+router.put("/customers/customer/:id", function (req, res) {
   let id = req.params.id;
   //get the customer by the id
   let sqlGetCustomer = `SELECT  * FROM customers WHERE id = '${id}'`;
@@ -109,14 +105,14 @@ router.put("/customers/customer/:id", function(req, res) {
     console.log("id", id);
     let { title, first_name, surname, email } = req.body;
     //update if values are different
-     if (Array.isArray(customer) && customer.length === 1){
-      title === undefined  ? title = customer[0].title : title;
-       console.log('hi')
+    if (Array.isArray(customer) && customer.length === 1) {
+      title === undefined ? title = customer[0].title : title;
+      console.log('hi')
       first_name === undefined ? first_name = customer[0].first_name : first_name;
       surname === undefined ? surname = customer[0].surname : surname
       email === undefined ? email = customer[0].email : email
-     }
-   
+    }
+
     let data = [title, first_name, surname, email, id];
     console.log("data:", data);
     let sql = `UPDATE customers 
@@ -126,7 +122,7 @@ router.put("/customers/customer/:id", function(req, res) {
               surname = ?,
               email = ?
              WHERE id = ?`;
-    db.run(sql, data, function(err) {
+    db.run(sql, data, function (err) {
       if (err) {
         return console.error(err.message);
       }
@@ -135,10 +131,19 @@ router.put("/customers/customer/:id", function(req, res) {
     });
   });
 });
+//reservations
 
-
-// get '/reservations'
-router.get("/reservations", function(req, res) {
+//post reservation:HOMEWORK 2
+router.post("/reservations/", function (req, res) {
+  const { customer_id, room_id, check_in_date, check_out_date, room_price } = req.body;
+  console.log(req.body);
+  var sql = `INSERT INTO reservations(customer_id, room_id, check_in_date, check_out_date, room_price) VALUES 
+  ('${customer_id}', '${room_id}', '${check_in_date}', '${check_out_date}', '${room_price}')`;
+  db.run(sql);
+  return res.status(201).send();
+});
+// get '/reservations': HOMEWORK 3
+router.get("/reservations", function (req, res) {
   var sql = "select * from reservations";
   // var sql = `select * from customers where surname in ("O'Connor", 'Trump')`;
   db.all(sql, [], (err, rows) => {
@@ -153,13 +158,34 @@ router.get("/reservations", function(req, res) {
   });
 });
 
-
-// get '/reservations/:id'
-// TODO: add code here
-
+// get '/reservations/:id': HOMEWORK 4
+router.get("/reservations/:id", function(req, res) {
+  // TODO: add code here
+  const id = req.params.id;
+  console.log(id);
+  var regx = new RegExp("^[0-9]{1,}$");
+  console.log(regx.test(id));
+  if (!regx.test(id)) {
+    return res.status(400).json({ reservations: "bad request!" });
+  }
+  // var sql = `select * from reservations where id = +${id}`;
+  var sql = "select * from reservations";
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.log("ERROR fetching from the database:", err);
+      return;
+    }
+    console.log(rows);
+    const reservation = rows.filter(reservation => reservation.id === +id);
+    console.log("Request succeeded, new data fetched", reservation);
+    res.status(200).json({
+      reservations: reservation
+    });
+  });
+});
 
 // delete '/reservations/:id'
-router.delete("/reservations/:id", function(req, res) {
+router.delete("/reservations/:id", function (req, res) {
   // const { title, first_name, surname, email } = req.body
   console.log(req.params.id);
   var sql = `delete from reservations where id = ${req.params.id}`;
--- database/schema.sql
+++ database/schema.sql
@@ -9,8 +9,8 @@ IF NOT EXISTS customers
   surname TEXT(64) NOT NULL, 
   email TEXT(64)
 );
-
-INSERT INTO customers
+-- 7 customers
+  INSERT INTO customers
   (title, first_name, surname, email) VALUES ('Mr', 'John', 'Dove', 'john.doe@domain.com');
   INSERT INTO customers
   (title, first_name, surname, email) VALUES ('Mr', 'Jim', 'Frlton', 'jim.f@domain.com');
@@ -22,8 +22,6 @@ INSERT INTO customers (title, first_name, surname, email) VALUES ('Ms',    'Anna
 INSERT INTO customers (title, first_name, surname, email) VALUES ('Ms',    'Marie','Niki','m.niki@domain.com');
 INSERT INTO customers (title, first_name, surname, email) VALUES ('Ms',    'Cherie','Mart','c.mart@domain.com');
 
-select * from customers;
-
 DROP table IF EXISTS room_types;
 CREATE TABLE
 IF NOT EXISTS room_types
@@ -34,19 +32,14 @@ IF NOT EXISTS room_types
   current_price number
 );
 
-
-
+-- 3 room_types
 INSERT INTO room_types (type_name, original_price, current_price) VALUES ('Single room', 40, 50.50 );
 
 INSERT INTO room_types (type_name, original_price, current_price) VALUES ('Double room', 60, 80 );
 
 INSERT INTO room_types (type_name, original_price, current_price) VALUES ('Suite', 100, 120 );
 
-select * from room_types;
-
-
 DROP table IF EXISTS rooms;
-
 CREATE TABLE
 IF NOT EXISTS rooms
 (
@@ -54,7 +47,7 @@ IF NOT EXISTS rooms
   seaview boolean,
   foreign key(room_type_id) references room_types(id)
 );
-
+-- 7 rooms
 INSERT INTO rooms (room_type_id, seaview) VALUES (1, 0);
 INSERT INTO rooms (room_type_id, seaview) VALUES (2, 0);
 INSERT INTO rooms (room_type_id, seaview) VALUES (2, 1);
@@ -63,11 +56,7 @@ INSERT INTO rooms (room_type_id, seaview) VALUES (2, 0);
 INSERT INTO rooms (room_type_id, seaview) VALUES (2, 1);
 INSERT INTO rooms (room_type_id, seaview) VALUES (1, 1);
 
-
-select * from rooms;
-
 DROP table IF EXISTS reservations;
-
 create table IF NOT EXISTS reservations (
       id integer primary key AUTOINCREMENT ,
       customer_id integer not null,
@@ -82,21 +71,17 @@ foreign key
 (room_id)     references rooms
 (id)
   );
-
+-- 7 reservations
   INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (1,2,'2018/07/10', 100);
   INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (2,3,'2018/07/10', 200);
   INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (3,4,'2018/07/10', 300);
   INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (4,1,'2018/07/10', 100);
-
   INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (1,2,'2018/07/10', 200);
   INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (2,3,'2018/07/10', 500);
   INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (3,4,'2018/07/10', 200);
   INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (4,1,'2018/07/10', 700);
-
-select * from reservations;
-
+  INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (5,1,'2018/07/10', 1700);
 DROP table IF EXISTS invoices;
-
 create table IF NOT EXISTS invoices (
       id integer primary key AUTOINCREMENT ,
       reservation_id integer not null,
@@ -108,16 +93,14 @@ create table IF NOT EXISTS invoices (
 (reservation_id)     references reservations
 (id)
   );
+-- 5 invoices
 INSERT INTO invoices (reservation_id, total, surcharges, invoice_date_time, paid) VALUES (1, 700, 50, '2018/05/01',1);
 INSERT INTO invoices (reservation_id, total, surcharges, invoice_date_time, paid) VALUES (2, 1000, 550, '2018/06/08',0);
 INSERT INTO invoices (reservation_id, total, surcharges, invoice_date_time, paid) VALUES (3, 200, 50, '2018/03/25',1);
 INSERT INTO invoices (reservation_id, total, surcharges, invoice_date_time, paid) VALUES (4, 7700, 50, '2018/08/07',1);
 INSERT INTO invoices (reservation_id, total, surcharges, invoice_date_time, paid) VALUES (5, 600, 50, '2018/04/14',0);
 
-select * from invoices;
-
 DROP table IF EXISTS reviews;
-
 create table IF NOT EXISTS reviews (
       id integer primary key AUTOINCREMENT ,
       customer_id integer not null,
@@ -132,8 +115,8 @@ foreign key
 (room_type_id)     references room_types
 (id)
   );
+  -- 4 reviews
   INSERT into reviews (customer_id, room_type_id, rating, comment, review_date) VALUES (1, 2, 4, "Great room and beautiful hotel", '2018/05/01');
   INSERT into reviews (customer_id, room_type_id, rating, comment, review_date) VALUES (2, 3, 3, "Beautiful hotel", '2018/05/01');
   INSERT into reviews (customer_id, room_type_id, rating, comment, review_date) VALUES (4, 1, 5, "Wonderful service", '2018/05/01');
   INSERT into reviews (customer_id, room_type_id, rating, comment, review_date) VALUES (3, 1, 4, "Great hotel", '2018/05/01');
-  select * from reviews;
